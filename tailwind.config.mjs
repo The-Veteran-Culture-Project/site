2354811d9b3d0b9ea3ef519c23c47a8b0ddca3bb
Stephen Onnen <stephen.onnen@gmail.com>
2024-05-15 10:35:16 -0500

    fonts and things

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,11 @@ import typography from "@tailwindcss/typography";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: ["Titillium Web", "system-ui", "sans-serif"],
+      display: ["Open Sans", "system-ui", "sans-serif"],
+      static: ["Bebas Neue", "system-ui", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [typography()],
