97216f5deef8655322af541ba64854c829d29935
Stephen Onnen <stephen.onnen@gmail.com>
2024-09-06 21:16:15 -0500

    team card changes

--- src/components/TeamMemberCard.tsx
+++ src/components/TeamMemberCard.tsx
@@ -11,7 +11,7 @@ type Props = {
 export const TeamMemberCard = ({ name, title, bio, avatar }: Props) => {
   console.log(avatar);
   return (
-    <Card className="w-full max-w-md flex flex-grow flex-col m-6">
+    <Card className="w-full max-w-md flex flex-grow flex-col m-6 drop-shadow-md">
       <CardHeader className="flex flex-row items-center gap-4 space-y-0">
         <Avatar className="h-24 w-24">
           <AvatarImage alt={name} src={avatar} />
@@ -24,11 +24,11 @@ export const TeamMemberCard = ({ name, title, bio, avatar }: Props) => {
         </Avatar>
         <div className="flex flex-col">
           <h2 className="text-2xl font-bold">{name}</h2>
-          <p className="text-sm text-muted-foreground">{title}</p>
+          <p className="text-md text-muted-foreground font-semibold">{title}</p>
         </div>
       </CardHeader>
       <CardContent>
-        <p className="text-sm text-muted-foreground">{bio}</p>
+        <p className="text-sm text-muted-foreground font-display">{bio}</p>
       </CardContent>
     </Card>
   );
