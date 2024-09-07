1d4197bbf51b9e8fe25cffc2c99e636fe8f8363d
Stephen Onnen <stephen.onnen@gmail.com>
2024-09-07 12:41:21 -0500

    prerelease for middleware

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -2,10 +2,10 @@ export async function onRequest(context) {
   const {
     request, // same as existing Worker API
     env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
+    // params, // if filename includes [id] or [[path]]
+    // waitUntil, // same as ctx.waitUntil in existing Worker API
+    // next, // used for middleware or to fetch assets
+    // data, // arbitrary space for passing data between middlewares
   } = context;
 
   const client_id = env.GITHUB_CLIENT_ID;
@@ -18,7 +18,7 @@ export async function onRequest(context) {
     redirectUrl.searchParams.set("scope", "repo user");
     redirectUrl.searchParams.set(
       "state",
-      crypto.getRandomValues(new Uint8Array(12)).join(""),
+      crypto.getRandomValues(new Uint8Array(12)).join("")
     );
     return Response.redirect(redirectUrl.href, 301);
   } catch (error) {
