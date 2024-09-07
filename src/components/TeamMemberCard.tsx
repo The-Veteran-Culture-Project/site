bb1ca436363d03cfcf33723200f0493fd620de07
Stephen Onnen <stephen.onnen@gmail.com>
2024-09-06 21:45:41 -0500

    update cards

--- src/components/TeamMemberCard.tsx
+++ src/components/TeamMemberCard.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 
@@ -6,22 +7,54 @@ type Props = {
   title: string;
   bio: string;
   avatar: string;
+  avatar2?: string;
 };
 
-export const TeamMemberCard = ({ name, title, bio, avatar }: Props) => {
-  console.log(avatar);
+export function TeamMemberCard({ name, title, bio, avatar, avatar2 }: Props) {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseEnter = () => setIsHovering(true);
+  const handleMouseLeave = () => setIsHovering(false);
+
   return (
-    <Card className="w-full max-w-md flex flex-grow flex-col m-6 drop-shadow-md">
+    <Card
+      className="w-full max-w-md flex flex-grow flex-col m-6 drop-shadow-md transition-all duration-300 ease-in-out hover:shadow-lg hover:bg-muted"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <CardHeader className="flex flex-row items-center gap-4 space-y-0">
-        <Avatar className="h-24 w-24">
-          <AvatarImage alt={name} src={avatar} />
-          <AvatarFallback>
-            {name
-              .split(" ")
-              .map((n) => n[0])
-              .join("")}
-          </AvatarFallback>
-        </Avatar>
+        <div
+          className="relative h-24 w-24"
+          role="img"
+          aria-label={`Avatar of ${name}`}
+        >
+          <Avatar
+            className={`absolute inset-0 transition-opacity duration-300 ease-in-out h-24 w-24 ${
+              isHovering ? "opacity-0" : "opacity-100"
+            }`}
+          >
+            <AvatarImage alt={`${name}'s primary avatar`} src={avatar} />
+            <AvatarFallback>
+              {name
+                .split(" ")
+                .map((n) => n[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+          <Avatar
+            className={`absolute inset-0 transition-opacity duration-300 ease-in-out h-24 w-24 ${
+              isHovering ? "opacity-100" : "opacity-0"
+            }`}
+          >
+            <AvatarImage alt={`${name}'s secondary avatar`} src={avatar2} />
+            <AvatarFallback>
+              {name
+                .split(" ")
+                .map((n) => n[0])
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+        </div>
         <div className="flex flex-col">
           <h2 className="text-2xl font-bold">{name}</h2>
           <p className="text-md text-muted-foreground font-semibold">{title}</p>
@@ -32,4 +65,4 @@ export const TeamMemberCard = ({ name, title, bio, avatar }: Props) => {
       </CardContent>
     </Card>
   );
-};
+}
