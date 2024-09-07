14b315e0dbdbb99f689110d793eb07e37bfd18ca
Stephen Onnen <stephen.onnen@gmail.com>
2024-09-07 14:51:12 -0500

    static rendering

--- functions/api/callback.js
+++ functions/api/callback.js
@@ -20,10 +20,10 @@ export async function onRequest(context) {
   const {
     request, // same as existing Worker API
     env, // same as existing Worker API
-    params, // if filename includes [id] or [[path]]
-    waitUntil, // same as ctx.waitUntil in existing Worker API
-    next, // used for middleware or to fetch assets
-    data, // arbitrary space for passing data between middlewares
+    // params, // if filename includes [id] or [[path]]
+    // waitUntil, // same as ctx.waitUntil in existing Worker API
+    // next, // used for middleware or to fetch assets
+    // data, // arbitrary space for passing data between middlewares
   } = context;
 
   const client_id = env.GITHUB_CLIENT_ID;
@@ -42,7 +42,7 @@ export async function onRequest(context) {
           accept: "application/json",
         },
         body: JSON.stringify({ client_id, client_secret, code }),
-      },
+      }
     );
     const result = await response.json();
     if (result.error) {
