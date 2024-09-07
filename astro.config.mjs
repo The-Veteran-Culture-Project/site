c4e73b304b8919eeaa25311b090b8ef493535aa2
Stephen Onnen <stephen.onnen@gmail.com>
2024-09-07 15:52:19 -0500

    deploy things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import cloudflare from "@astrojs/cloudflare";
 
 import db from "@astrojs/db";
@@ -22,11 +20,6 @@ export default defineConfig({
   vite: {
     optimizeDeps: {
       exclude: ["astro:db"],
-    },
-    define: {
-      "process.env.ASTRO_STUDIO_APP_TOKEN": JSON.stringify(
-        "2cb35d291538f2374269346d7dad53d6a1a0e381:gbait2oe8iuyu0xmu4c73i23os5r:gbait2oe8iuyu0xmu4c73i23os5r"
-      ),
     },
   },
   adapter: cloudflare({
