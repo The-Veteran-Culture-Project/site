7cd861d7091dee9c79a81364d6c34d2e630326ae
Stephen Onnen <stephen.onnen@gmail.com>
2024-09-07 20:35:51 -0500

    remove logs (#141)

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -44,9 +44,7 @@ export async function POST(context: APIContext): Promise<Response> {
       status: 400,
     });
   }
-  console.log(existingUser.password);
   const validPassword = password === existingUser.password;
-  console.log(password, existingUser.password);
 
   if (!validPassword) {
     return new Response("Incorrect username or password", {
