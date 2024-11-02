864d1530b30dedbce928165c20fc2d8b8a0d37ee
Stephen Onnen <stephen.onnen@gmail.com>
2024-11-01 21:15:43 -0500

    try this

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,9 @@ export default defineConfig({
     },
     define: {
       "process.env.ASTRO_DB_APP_TOKEN": JSON.stringify("PLACEHOLDER"),
-      "process.env.ASTRO_DB_REMOTE_URL": "libsql://vcpsite-onnenon.turso.io",
+      "process.env.ASTRO_DB_REMOTE_URL": JSON.stringify(
+        "libsql://vcpsite-onnenon.turso.io"
+      ),
     },
   },
   adapter: cloudflare({
